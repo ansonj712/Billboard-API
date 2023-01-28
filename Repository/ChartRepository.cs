2b3f1b844d1de11168b72637b9d0f00a3ad76055
ansonj712 <anson.jiang712@gmail.com>
2023-01-28 00:18:42 -0500

    Updated selectors

--- Repository/ChartRepository.cs
+++ Repository/ChartRepository.cs
@@ -11,26 +11,31 @@ namespace BillboardAPI.Repository
     public class ChartRepository
     {
         // Selectors
-        const string ChartListItemSelector = ".chart-list__element";
-        const string RankSelector = ".chart-element__rank__number";
-        const string SongSelector = ".chart-element__information__song";
-        const string ArtistSelector = ".chart-element__information__artist";
-        const string LastWeekSelector = ".chart-element__metas .text--last";
-        const string PeakSelector = ".chart-element__metas .text--peak";
-        const string DurationSelector = ".chart-element__metas .text--week";
-
-        const string ChartListItemSelectorAlt = ".chart-list-item";
-        const string RankSelectorAlt = ".chart-list-item__rank";
-        const string SongSelectorAlt = ".chart-list-item__title-text";
-        const string ArtistSelectorAlt = ".chart-list-item__artist";
-        const string LastWeekSelectorAlt = ".chart-list-item__last-week";
-        const string PeakSelectorAlt = ".chart-list-item__weeks-at-one";
-        const string DurationSelectorAlt = ".chart-list-item__weeks-on-chart";
+        const string ChartListItemSelector = ".o-chart-results-list-row-container";
+        const string RankSelector = ".c-label";
+        const string SongSelector = ".o-chart-results-list__item > #title-of-a-story";
+        const string ArtistSelector = ".lrv-a-unstyle-list > li:nth-child(4) li .c-label";
+        const string LastWeekSelector = ".lrv-a-unstyle-list > li:nth-child(4) > ul > li:nth-child(4)";
+        const string PeakSelector = ".lrv-a-unstyle-list > li:nth-child(4) > ul > li:nth-child(5)";
+        const string DurationSelector = ".lrv-a-unstyle-list > li:nth-child(4) > ul > li:nth-child(6)";
 
         public static List<ChartListItem> GetChart(string name)
         {
-            List<ChartListItem> chart = new List<ChartListItem>();
             string url = Flurl.Url.Combine("https://www.billboard.com/charts", name);
+
+            return ParseWebpage(url);
+        }
+
+        public static List<ChartListItem> GetChart(string name, string date)
+        {
+            string url = Flurl.Url.Combine("https://www.billboard.com/charts", name, date);
+
+            return ParseWebpage(url);
+        }
+
+        private static List<ChartListItem> ParseWebpage(string url)
+        {
+            List<ChartListItem> chart = new List<ChartListItem>();
 
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
@@ -38,21 +43,16 @@ namespace BillboardAPI.Repository
 
             List<HtmlNode> chartListItemNodes = doc.DocumentNode.QuerySelectorAll(ChartListItemSelector).ToList();
 
-            if (chartListItemNodes.Count == 0)
-            {
-                chartListItemNodes = doc.DocumentNode.QuerySelectorAll(ChartListItemSelectorAlt).ToList();
-            }
-
             foreach (var chartListItemNode in chartListItemNodes)
             {
                 ChartListItem chartListItem = new ChartListItem();
 
-                string rank;
-                string song;
-                string artist;
-                string lastWeek;
-                string peak;
-                string duration;
+                string rank = "";
+                string song = "";
+                string artist = "";
+                string lastWeek = "";
+                string peak = "";
+                string duration = "";
 
                 try
                 {
@@ -63,73 +63,9 @@ namespace BillboardAPI.Repository
                     peak = chartListItemNode.QuerySelector(PeakSelector).InnerText.Trim();
                     duration = chartListItemNode.QuerySelector(DurationSelector).InnerText.Trim();
                 }
-                catch
+                catch (Exception ex)
                 {
-                    rank = chartListItemNode.QuerySelector(RankSelectorAlt).InnerText.Trim();
-                    song = chartListItemNode.QuerySelector(SongSelectorAlt).InnerText.Trim();
-                    artist = chartListItemNode.QuerySelector(ArtistSelectorAlt).InnerText.Trim();
-                    lastWeek = chartListItemNode.QuerySelector(LastWeekSelectorAlt).InnerText.Trim();
-                    peak = chartListItemNode.QuerySelector(PeakSelectorAlt).InnerText.Trim();
-                    duration = chartListItemNode.QuerySelector(DurationSelectorAlt).InnerText.Trim();
-                }
-
-                chartListItem.Rank = Convert.ToInt32(rank);
-                chartListItem.Song = HtmlEntity.DeEntitize(song);
-                chartListItem.Artist = HtmlEntity.DeEntitize(artist);
-                chartListItem.LastWeek = lastWeek != "-" ? Convert.ToInt32(lastWeek) : 0;
-                chartListItem.Peak = Convert.ToInt32(peak);
-                chartListItem.Duration = Convert.ToInt32(duration);
-
-                chart.Add(chartListItem);
-            }
-
-            return chart;
-        }
-
-        public static List<ChartListItem> GetChart(string name, string date)
-        {
-            List<ChartListItem> chart = new List<ChartListItem>();
-            string url = Flurl.Url.Combine("https://www.billboard.com/charts", name, date);
-
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load(url);
-            web.OverrideEncoding = Encoding.UTF8;
-
-            List<HtmlNode> chartListItemNodes = doc.DocumentNode.QuerySelectorAll(ChartListItemSelector).ToList();
-
-            if(chartListItemNodes.Count == 0) 
-            {
-                chartListItemNodes = doc.DocumentNode.QuerySelectorAll(ChartListItemSelectorAlt).ToList();
-            }
-
-            foreach(var chartListItemNode in chartListItemNodes)
-            {
-                ChartListItem chartListItem = new ChartListItem();
-
-                string rank;
-                string song;
-                string artist;
-                string lastWeek;
-                string peak;
-                string duration;
-
-                try 
-                {
-                    rank = chartListItemNode.QuerySelector(RankSelector).InnerText.Trim();
-                    song = chartListItemNode.QuerySelector(SongSelector).InnerText.Trim();
-                    artist = chartListItemNode.QuerySelector(ArtistSelector).InnerText.Trim();
-                    lastWeek = chartListItemNode.QuerySelector(LastWeekSelector).InnerText.Trim();
-                    peak = chartListItemNode.QuerySelector(PeakSelector).InnerText.Trim();
-                    duration = chartListItemNode.QuerySelector(DurationSelector).InnerText.Trim();
-                }
-                catch
-                {
-                    rank = chartListItemNode.QuerySelector(RankSelectorAlt).InnerText.Trim();
-                    song = chartListItemNode.QuerySelector(SongSelectorAlt).InnerText.Trim();
-                    artist = chartListItemNode.QuerySelector(ArtistSelectorAlt).InnerText.Trim();
-                    lastWeek = chartListItemNode.QuerySelector(LastWeekSelectorAlt).InnerText.Trim();
-                    peak = chartListItemNode.QuerySelector(PeakSelectorAlt).InnerText.Trim();
-                    duration = chartListItemNode.QuerySelector(DurationSelectorAlt).InnerText.Trim();
+                    Console.WriteLine(ex.Message);
                 }
 
                 chartListItem.Rank = Convert.ToInt32(rank);
